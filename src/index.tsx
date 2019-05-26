3b74f66140ef12be5db6251fbe0629927df3303a
Edward <edylennyy@gmail.com>
2019-05-26 18:10:34 +0300

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { observable, action } from 'mobx';
+import { observer } from 'mobx-react';
+import DevTools from 'mobx-react-devtools';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class MobxData {
+  @observable clickedCount = 0;
+
+  @action
+  increment() {
+    this.clickedCount++;
+  }
+}
+
+@observer
+class ReactPlusMobx extends React.Component<{}> {
+  data = new MobxData();
+  render() {
+    return (
+      <button onClick={() => this.data.increment()}>
+        Click count: {this.data.clickedCount}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <>
+  <ReactPlusMobx />
+  <DevTools />
+  </>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
